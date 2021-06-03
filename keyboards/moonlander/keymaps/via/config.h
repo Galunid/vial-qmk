288a8d406eeb00350f2fc3946a8cd1a5b9777e7e
Galunid <karolek1231456@gmail.com>
2021-06-03 22:06:30 +0200

    Moonlander support

---
+++ keyboards/moonlander/keymaps/via/config.h
@@ -0,0 +1,12 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 150
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define RGB_MATRIX_STARTUP_SPD 60
+
+#define VIAL_KEYBOARD_UID {0xE9, 0x5C, 0x14, 0x3A, 0x24, 0xE2, 0x45, 0x5A}
